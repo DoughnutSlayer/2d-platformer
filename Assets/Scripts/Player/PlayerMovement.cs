ba278dbb71a74299b5a1bb75ef8b234df652d389
DoughnutSlayer <nikolai.elich@gmail.com>
2016-07-19 05:06:27 +0200

    Overhauled player jumping

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,48 +12,30 @@
     /// <version>19.06.2016</version>
     public class PlayerMovement : MonoBehaviour
     {
-        public int _movementSpeed;          // The Player's movement speed.
-        public int _jumpingPower;           // The Player's jumping Power.
-        public int _wallDropSpeed;          // How fast the player drops when pressing against a wall.
+        public int _movementSpeed; // The Player's movement speed.
+        public int _jumpingPower; // The Player's jumping Power.
+        public int _inputBuffer; // The number of fixed Updates for which the jump input will be buffered. Intentionally does not work for walljumps.
+        public int _wallDropSpeed; // How fast the player drops when pressing against a wall.
+        public int _jumpStopForceMultiplier; // Determines how strong the force is, that cuts a jump short.
 
-        private bool _jumping;              // Wether the player is in mid-jump.
-        private bool _jumpPressed;          // Becomes true when the player presses the Jump-Button, and false again, when the jump is started in the next FixedUpdate().
+        private Rigidbody2D _rb2D; // The Player GameObject's Rigidbody.
 
-        private Rigidbody2D _rb2D;          // The Player GameObject's Rigidbody.
-
-        private RectangleHitbox _hitbox;    // The player's hitbox.
+        private RectangleHitbox _hitbox; // The player's hitbox.
 
         /// <summary>
         /// Initializes the fields. The public fields are to be initialized in the Inspector.
         /// </summary>
         void Start()
         {
-            _jumping = false;
-            _jumpPressed = false;
-
             _rb2D = GetComponentInParent<Rigidbody2D>();
             _hitbox = _rb2D.GetComponentInChildren<RectangleHitbox>();
         }
 
         void Update()
         {
-            checkIfStillJumping();
-            setJumpInput();
-        }
-
-        private void checkIfStillJumping()
-        {
-            if (_jumping)
+            if (Input.GetButtonDown("Jump"))
             {
-                _jumping = !(_hitbox._touchingFloor || _hitbox.touchingWall());
-            }
-        }
-
-        private void setJumpInput()
-        {
-            if (!(_jumping || _jumpPressed))
-            {
-                _jumpPressed = Input.GetButtonDown("Jump") && (_hitbox._touchingFloor || _hitbox.touchingWall());   //Walljumping is enabled.
+                StartCoroutine("attemptJump");
             }
         }
 
@@ -63,15 +45,6 @@
         }
 
         private void setVelocity()
-        {
-            if (_jumpPressed)
-            {
-                Jump();
-            }
-            setHorizontalVelocity();
-        }
-
-        private void setHorizontalVelocity()
         {
             int horizontalInput = (int)Input.GetAxisRaw("Horizontal");
 
@@ -96,11 +69,32 @@
             }
         }
 
-        private void Jump()
+        private IEnumerator attemptJump()
+        {
+            if (_hitbox._touchingFloor || _hitbox.touchingWall())
+            {
+                jump();
+                yield break;
+            }
+            for (int i = 0; i < _inputBuffer; i++)
+            {
+                if (_hitbox._touchingFloor)
+                {
+                    jump();
+                    yield break;
+                }
+                if (i == _inputBuffer - 1)
+                {
+                    yield break;
+                }
+                yield return new WaitForFixedUpdate();
+            }
+        }
+
+        private void jump()
         {
             _rb2D.velocity = new Vector2(_rb2D.velocity.x, 0);
             _rb2D.AddForce(new Vector2(0, _jumpingPower));
-            _jumpPressed = false;
             StartCoroutine("stopJump");
         }
 
@@ -111,13 +105,12 @@
         {
             yield return new WaitForFixedUpdate();
             yield return new WaitForFixedUpdate();
-            _jumping = true;
-            yield return new WaitWhile(() => { return _jumping && (Input.GetButton("Jump") || _rb2D.velocity.y <= 0); } );
+            yield return new WaitWhile(() => { return Input.GetButton("Jump") && _rb2D.velocity.y > 0; });
             yield return new WaitForFixedUpdate();
 
-            if (_rb2D.velocity.y > 0 && _jumping)
+            if (_rb2D.velocity.y > 0)
             {
-                _rb2D.velocity = new Vector2(_rb2D.velocity.x, 0);
+                _rb2D.AddForce(new Vector2(0, -_rb2D.velocity.y * _jumpStopForceMultiplier));
             }
         }
     }
